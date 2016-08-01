58599ee093c7391f4b67c1abf08ba7496cb98a4e
luboganchev <luboganchev91@gmail.com>
2016-08-01 20:29:39 +0300

    Fix missing files

---
+++ TwitterBackup/TwitterBackup.Models/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterBackup.Models
+{
+    public interface IEntity
+    {
+        string Id { get; set; }
+    }
+}
---
+++ TwitterBackup/TwitterBackup.Models/Tweet.cs
@@ -0,0 +1,25 @@
+﻿namespace TwitterBackup.Models
+{
+    using System;
+
+    public class Tweet : IEntity
+    {
+        public string Id { get; set; }
+
+        public string CreatedByName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Text { get; set; }
+
+        public string FullText { get; set; }
+
+        public int FavoriteCount { get; set; }
+
+        public int RetweetCount { get; set; }
+
+        public bool Retweeted { get; set; }
+
+        //public TweetViewModel Retweet { get; set; }
+    }
+}
