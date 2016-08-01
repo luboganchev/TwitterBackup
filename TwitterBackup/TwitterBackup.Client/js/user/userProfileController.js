9a44d859be1faeed6e39f331569f7b9e46fcf38d
luboganchev <luboganchev91@gmail.com>
2016-08-02 00:33:15 +0300

    Angular user profile added

---
+++ TwitterBackup/TwitterBackup.Client/js/user/userProfileController.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    function UserProfileController($scope, $location, $routeParams, userService) {
+        var vm = this;
+        vm.id = $routeParams.id;
+        $scope.userProfile = null;
+        userService.getUserDetails(vm.id)
+            .then(function (response) {
+                $scope.userProfile = JSON.parse(response);
+            });
+    }
+
+    angular.module('myApp.controllers')
+        .controller('UserProfileController', ['$scope', '$location', '$routeParams', 'userService', UserProfileController]);
+}());
