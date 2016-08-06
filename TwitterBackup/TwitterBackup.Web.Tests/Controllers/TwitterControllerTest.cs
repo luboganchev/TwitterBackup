431ada2a2706ad0299f170ccf9c329502ee6aa53
luboganchev <luboganchev91@gmail.com>
2016-08-06 22:40:01 +0300

    Add inital twittercontroller test + mock tweet service

---
+++ TwitterBackup/TwitterBackup.Web.Tests/Controllers/TwitterControllerTest.cs
@@ -0,0 +1,42 @@
+﻿namespace TwitterBackup.Web.Tests.Controllers
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Configuration;
+    using System.Web.Http;
+    using System.Web.Http.Results;
+    using TwitterBackup.Services.Contracts;
+    using TwitterBackup.Web.Controllers;
+    using TwitterBackup.Web.Helpers;
+
+    [TestClass]
+    public class TwitterControllerTest
+    {
+        private ITweetService tweetService;
+        private IRetweetService retweetService;
+        private IUserService userService;
+
+        [TestInitialize]
+        public void Init()
+        {
+            this.tweetService = MockObjectFactory.GetTweetService();
+            this.retweetService = MockObjectFactory.GetRetweetService();
+            this.userService = MockObjectFactory.GetUserService();
+        }
+
+        [TestMethod]
+        public void StoreTweetShouldReturnBadRequestWithInvalidModel()
+        {
+            var controller = new TwitterController(this.tweetService, this.retweetService, this.userService);
+            controller.Configuration = new HttpConfiguration();
+            var invalidModel = MockObjectFactory.GetInvalidModel();
+            controller.Validate(invalidModel);
+
+            var result = controller.StoreTweet(invalidModel);
+            var badResult = result as BadRequestErrorMessageResult;
+
+            Assert.IsFalse(controller.ModelState.IsValid);
+            Assert.IsNotNull(badResult);
+            Assert.AreEqual("Tweet that you're trying to save has some invalid arguments", badResult.Message);
+        }
+    }
+}
---
+++ TwitterBackup/TwitterBackup.Web.Tests/MockObjectFactory.cs
@@ -0,0 +1,77 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterBackup.Models;
+using TwitterBackup.Services.Contracts;
+using TwitterBackup.Web.Models.Tweets;
+
+namespace TwitterBackup.Web.Tests
+{
+    public static class MockObjectFactory
+    {
+        private static List<Tweet> tweets = new List<Tweet>
+        {
+            new Tweet()
+            {
+                Id = Guid.NewGuid().ToString(),
+                TweetTwitterId = 132456789,
+                Text = "Sample text",
+                CreatedAt = DateTime.Now,
+                RetweetCount = 2,
+                FavoriteCount = 4,
+                FullText = "Sample text Sample text",
+                CreatedById = 123465789,
+                RetweetedFromMe = false
+            }
+        };
+
+        public static ITweetService GetTweetService()
+        {
+            var tweetService = new Mock<ITweetService>();
+
+            tweetService.Setup(ts => ts.GetTotalTweetsCount())
+                .Returns(tweets.Count);
+
+            tweetService.Setup(ts => ts.GetTweets())
+                .Returns(tweets);
+
+            tweetService.Setup(ts => ts.GetTweetsForFriend(
+                    It.Is<long>(s => s == 123456),
+                    It.Is<long>(s => s == 654321)))
+                .Returns(tweets);
+
+            tweetService.Setup(ts => ts.GetTweetsForFriend(
+                    It.Is<long>(s => s != 123456),
+                    It.Is<long>(s => s != 654321)))
+                .Returns(new List<Tweet>());
+
+            tweetService.Setup(ts => ts.Save(
+                    It.IsAny<Tweet>()))
+                .Returns(tweets.FirstOrDefault());
+
+            return tweetService.Object;
+        }
+
+        public static IRetweetService GetRetweetService()
+        {
+            var retweetService = new Mock<IRetweetService>();
+
+            return retweetService.Object;
+        }
+
+        public static IUserService GetUserService()
+        {
+            var userService = new Mock<IUserService>();
+
+            return userService.Object;
+        }
+
+        public static TweetViewModel GetInvalidModel()
+        {
+            return new TweetViewModel { FullText = "Sample" };
+        }
+    }
+}
