fccca12d569d32bee47f28ca4177d9db1732257a
luboganchev <luboganchev91@gmail.com>
2016-08-08 18:34:35 +0300

    Remove useless server files

--- TwitterBackup/TwitterBackup.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace TwitterBackup.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- TwitterBackup/TwitterBackup.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace TwitterBackup.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
